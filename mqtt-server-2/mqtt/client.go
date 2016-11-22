da0a0f0272154445b7fd519b964a28eac9fee984
Giovanni Baleani <giovibal@gmail.com>
2016-11-22 23:19:51 +0100

    full compatibility with mqtt 3.1.1

--- mqtt-server-2/mqtt/client.go
+++ mqtt-server-2/mqtt/client.go
@@ -31,8 +31,8 @@ func NewClient(connection net.Conn) *Client {
 		reader:        reader,
 		writer:        writer,
 		queue:         NewQueue(),
-		incoming:      make(chan packets.ControlPacket, 40),
-		outgoing:      make(chan packets.ControlPacket, 40),
+		incoming:      make(chan packets.ControlPacket, 8),
+		outgoing:      make(chan packets.ControlPacket, 8),
 		Subscriptions: make(map[string]*Subscription),
 		quit:          make(chan bool, 4),
 	}
